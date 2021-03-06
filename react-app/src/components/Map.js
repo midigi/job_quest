a009e5cfca506528c8d12e4a8b4e6745f4c205e1
Michael DiGiovanni <midigi@comcast.net>
2021-03-05 19:13:08 -0500

    more mapping css

--- react-app/src/components/Map.js
+++ react-app/src/components/Map.js
@@ -60,14 +60,15 @@ function Maps() {
                 <div className="stadium_img"></div>
             </div> */}
             {/* style="background-image: url('../images/house_icon.png')" */}
-            <div className="map_tile">
-                <img className="house_img"/>
-                <div className='location_name'>Home</div>
+            <div className="map_tile house_img">
+                <div className='location_name'>
+                    House
+                </div>
             </div>
             <div className="map_tile">
                 <div className="blank_tile"></div>
             </div>
-            <div className="map_tile">
+            <div className="map_tile stadium_img">
                 <div className="stadium_img">
                     <div className='location_name'>
                         Stadium
@@ -77,17 +78,42 @@ function Maps() {
             <div className="map_tile">
                 <div className="blank_tile2"></div>
             </div>
-            <div className="map_tile">
-                <div className="library_img"></div>
+            <div className="map_tile library_img">
+                <div className="library_img">
+                    <div className='location_name'>
+                        Library
+                    </div>
+                </div>
             </div>
             <div className="map_tile">
                 <div className="blank_tile3"></div>
+            </div>
+            <div className="map_tile admission_img">
+                <div className="admission_img">
+                    <div className='location_name'>
+                        Admission's Office
+                    </div>
+                </div>
             </div>
             <div className="map_tile">
                 <div className="blank_tile4"></div>
             </div>
             <div className="map_tile">
                 <div className="blank_tile5"></div>
+            </div>
+            <div className="map_tile lecture_hall_img">
+                <div className="lecture_hall_img">
+                    <div className='location_name'>
+                        Lecture Hall
+                    </div>
+                </div>
+            </div>
+            <div className="map_tile career_office_img">
+                <div className="career_office_img">
+                    <div className='location_name'>
+                        Career Office
+                    </div>
+                </div>
             </div>
 
 
